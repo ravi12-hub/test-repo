d9e36eaf117664e567ecc27886c28d2918bfa45d
RAVI <ravisharma80500@gmail.com>
2026-04-24 14:05:53 +0530

    Add a age feautre

--- hello.sh
+++ hello.sh
@@ -2,3 +2,10 @@
 
 read -p "ENTER YOUR NAME: " username
 echo hello $username
+
+read -p "ENTER YOUR AGE: " age
+if [ $age -ge 18 ]; then
+   echo you are eligible to work
+else
+   echo you are not eligible
+fi
